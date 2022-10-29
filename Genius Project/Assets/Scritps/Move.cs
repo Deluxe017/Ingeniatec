0295c8a8ba103a642ae993dc5c0b28b60b08d295
Julian David <jdavid142@misena.edu.co>
2022-10-29 10:31:09 -0500

    Intentando que se mueva y flote :D
    No da ni con codigo ni con animacion de momento wuuuu

--- Genius Project/Assets/Scritps/Move.cs
+++ Genius Project/Assets/Scritps/Move.cs
@@ -19,6 +19,8 @@ public class Move : MonoBehaviour
     [SerializeField] private Transform controladorSuelo;
     [SerializeField] private Vector3 dimensionesCaja;
     [SerializeField] private bool enSuelo;
+
+    public Animator flotar;
    
 
 
@@ -33,7 +35,8 @@ public class Move : MonoBehaviour
     }
     private void Update()
     {
-      
+
+        flotar.SetBool("Flotar", true);
         movimientoHorizontal = Input.GetAxisRaw("Horizontal") * velocidadMovimiento;
 
         if (Input.GetButtonDown("Jump"))
