d038a5d39f8f18e3ac27d8e1ec2f6ca069ba7db4
Julian David <jdavid142@misena.edu.co>
2022-10-29 13:26:10 -0500

    Ya funciona el mover y la animacion tecnica rebuscadisima pero funciona

--- Genius Project/Assets/Scritps/Move.cs
+++ Genius Project/Assets/Scritps/Move.cs
@@ -20,23 +20,24 @@ public class Move : MonoBehaviour
     [SerializeField] private Vector3 dimensionesCaja;
     [SerializeField] private bool enSuelo;
 
-    public Animator flotar;
-   
+    [SerializeField] private float _amplitud = 1;
+    [SerializeField] private float _frecuencia = 0.1f;
 
-
+    private Vector3 posicion;
     private bool Salto = false;
 
 
     private void Start()
     {
         rg2D = GetComponent<Rigidbody2D>();
-        
-
+        posicion = transform.position;
     }
     private void Update()
     {
 
-        flotar.SetBool("Flotar", true);
+        /*float x = transform.position.x;
+        float y =  Mathf.Sin(Time.time * _frecuencia) * _amplitud;
+        transform.position = new Vector2(x, y);*/
         movimientoHorizontal = Input.GetAxisRaw("Horizontal") * velocidadMovimiento;
 
         if (Input.GetButtonDown("Jump"))
