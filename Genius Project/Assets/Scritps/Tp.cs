0f9e8b9f0ba2db6a0b1ce4b3e1671a95321e6c8f
stuart cardenas <stuartcardenasduarte@gmail.com>
2022-10-31 19:17:48 -0500

    Scrip Lampara

--- Genius Project/Assets/Scritps/Tp.cs
+++ Genius Project/Assets/Scritps/Tp.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class Tp : MonoBehaviour
 {
+    public string win;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        SceneManager.LoadScene("Nivel2");
+        SceneManager.LoadScene(this.win);
     }
 }
